bdd7732138536aff574766ba823ddcecb78a5563
Mohamed Ahmed <mohamedtaha2005@gmail.com>
2025-04-23 21:06:06 +0200

    Add Checkers utility class for GitHub token validation

---
+++ src/main/java/aiss/gitminer/util/Checkers.java
@@ -0,0 +1,16 @@
+package aiss.gitminer.util;
+
+import java.util.function.Predicate;
+
+public class Checkers {
+    /**
+     * Check if the given token is a valid GitHub token.
+     *
+     * @param token the token to check
+     * @return true if the token is a valid GitHub token, false otherwise
+     */
+    public static boolean isTokenGithub(String token) {
+        Predicate<String> isTokenGithub = t -> t != null && t.startsWith("ghp_");
+        return isTokenGithub.test(token);
+    }
+}
