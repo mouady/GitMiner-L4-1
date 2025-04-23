bc5cb73fe01fbd54870c25a87b68b176645a7586
Mohamed Ahmed <mohamedtaha2005@gmail.com>
2025-04-23 21:08:26 +0200

    Add Environment class for managing configuration variables

---
+++ src/main/java/aiss/gitminer/util/Environment.java
@@ -0,0 +1,24 @@
+package aiss.gitminer.util;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Environment {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    // GitMiner
+
+
+    // GitHub
+    public static final String GITHUB_TOKEN = dotenv.get("GITHUB_TOKEN");
+    public static final String GITHUB_BASEURI = dotenv.get("GITHUB_BASEURI");
+
+    public static final String GITHUB_DEFAULT_SINCE_COMMITS = DateUtils.getDateMinusDays(Integer.parseInt(dotenv.get("GITHUB_DEFAULT_SINCE_COMMITS")));
+    public static final String GITHUB_DEFAULT_SINCE_ISSUES = DateUtils.getDateMinusDays(Integer.parseInt(dotenv.get("GITHUB_DEFAULT_SINCE_ISSUES")));
+    public static final Integer GITHUB_DEFAULT_MAX_PAGES =Integer.valueOf(dotenv.get("GITHUB_DEFAULT_MAX_PAGES"));
+
+    // Bitbucket
+    public static final String BITBUCKET_TOKEN = dotenv.get("BITBUCKET_TOKEN");
+    public static final String BITBUCKET_BASEURI = dotenv.get("BITBUCKET_BASEURI");
+
+
+}
