880af03e2a148fe1c0a0c12bd67e8e2cabf73847
Mohamed Ahmed <mohamedtaha2005@gmail.com>
2025-04-24 20:06:07 +0200

    Fix CommitGithubTransformer to correctly handle committer information

--- src/main/java/aiss/gitminer/transformers/github/CommitGithubTransformer.java
+++ src/main/java/aiss/gitminer/transformers/github/CommitGithubTransformer.java
@@ -9,11 +9,17 @@ public class CommitGithubTransformer {
 
     public static Commit transformToCommit(CommitGithub commitGithub) {
         return new Commit(commitGithub.getSha(),
-                "",
+                "", // No tiene sentido title en este caso
                 commitGithub.getCommit().getMessage(),
                 commitGithub.getCommit().getAuthor().getName(),
                 commitGithub.getCommit().getAuthor().getEmail(),
                 commitGithub.getCommit().getAuthor().getDate(),
+
+                // Importante no seguir el patron: commitGithub.getCommit().getCommitter().[getName(),getEmail(),getDate()]
+                // NO: commitGithub.getCommitter().[getName(), getEmail(), getDate()]
+                commitGithub.getCommit().getCommitter().getName(),
+                commitGithub.getCommit().getCommitter().getEmail(),
+                commitGithub.getCommit().getCommitter().getDate(),
                 commitGithub.getUrl()
                 );
     }
