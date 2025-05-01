8165da877a6259f13f4fc5e43860191b9b08e7e9
AlejandroG2005 <alegarmon@alum.us.es>
2025-05-01 20:05:25 +0200

    ProjectsController

---
+++ src/main/java/aiss/gitminer/controller/ProjectsController.java
@@ -0,0 +1,25 @@
+package aiss.gitminer.controller;
+
+import aiss.gitminer.repository.ProjectRepository;
+import aiss.gitminer.model.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/gitminer")
+public class ProjectsController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @ResponseStatus(HttpStatus.CREATED) //201
+    @PostMapping
+    public Project create(@Valid @RequestBody Project project) {
+        return projectRepository.save(new Project(project.getId(),project.getName(),
+                project.getWebUrl(),project.getCommits(),project.getIssues()));
+    }
+
+}
