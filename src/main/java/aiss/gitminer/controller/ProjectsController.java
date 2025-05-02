5b17876a214cbe5c62f9d87c8039e0c48e01470b
AlejandroG2005 <alegarmon@alum.us.es>
2025-05-02 17:51:04 +0200

    Metodo PUT Project y etiquetas en los tests

--- src/main/java/aiss/gitminer/controller/ProjectsController.java
+++ src/main/java/aiss/gitminer/controller/ProjectsController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/gitminer")
@@ -14,11 +15,26 @@ public class ProjectsController {
 
     @Autowired
     ProjectRepository projectRepository;
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/projects")
-    @ResponseStatus(HttpStatus.CREATED) //201
     public Project create(@Valid @RequestBody Project project) {
         return projectRepository.save(new Project(project.getId(),project.getName(),
                 project.getWebUrl(),project.getCommits(),project.getIssues()));
     }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("projects/{id}")
+    public void update(@Valid @RequestBody Project updatedProject, @PathVariable String id) {
+        Optional<Project> projectData=projectRepository.findById(id);
+
+        Project _project = projectData.get();
+        _project.setName(updatedProject.getName());
+        _project.setWebUrl((updatedProject.getWebUrl()));
+        _project.setCommits(updatedProject.getCommits());
+        _project.setIssues(updatedProject.getIssues());
+        projectRepository.save(_project);
+    }
+;
 
 }
--- src/test/java/aiss/gitminer/controllers/ProjectsControllerTest.java
+++ src/test/java/aiss/gitminer/controllers/ProjectsControllerTest.java
@@ -1,12 +1,20 @@
 package aiss.gitminer.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 
 class ProjectsControllerTest {
 
+
+
     @Test
     void create() {
+    }
+
+    @Test
+    void update() {
     }
 }
