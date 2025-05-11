a18b9f55b2cd856ea35855a6343ce8268295f0b1
Mohamed Ahmed <mohamedtaha2005@gmail.com>
2025-05-11 16:23:18 +0200

    Refactor commit transformers to split message into title and body

--- src/main/java/aiss/gitminer/transformers/bitbucket/CommitBitbucketTransformer.java
+++ src/main/java/aiss/gitminer/transformers/bitbucket/CommitBitbucketTransformer.java
@@ -2,6 +2,7 @@ package aiss.gitminer.transformers.bitbucket;
 
 import aiss.gitminer.model.Commit;
 import aiss.gitminer.model.bitbucket.commit.CommitBitbucket;
+import aiss.gitminer.util.Utils;
 
 import java.util.List;
 
@@ -10,9 +11,11 @@ public class CommitBitbucketTransformer {
     public static Commit transformToCommit(CommitBitbucket commitBitbucket) {
         int aperturaCorreo = commitBitbucket.getAuthor().getRaw().indexOf('<');
         int cierreCorreo = commitBitbucket.getAuthor().getRaw().indexOf('>');
+        List<String> tituloYMensaje = Utils.splitIntoTitleAndMessage(commitBitbucket.getMessage());
+
         return new Commit(commitBitbucket.getHash(),
-                commitBitbucket.getSummary().getRaw(),
-                commitBitbucket.getMessage(),
+                tituloYMensaje.get(0),
+                tituloYMensaje.get(1),
                 commitBitbucket.getAuthor().getRaw().substring(0, aperturaCorreo).trim(),
                 commitBitbucket.getAuthor().getRaw().substring(aperturaCorreo + 1, cierreCorreo).trim(),
                 commitBitbucket.getDate(),
--- src/main/java/aiss/gitminer/util/Utils.java
+++ src/main/java/aiss/gitminer/util/Utils.java
@@ -16,4 +16,28 @@ public class Utils {
         LocalDate date = LocalDate.now().minusDays(days);
         return date.format(DateTimeFormatter.ISO_DATE);
     }
+
+    /**
+     * Divide un mensaje en un título y un cuerpo.
+     * El título es la primera línea del mensaje y el cuerpo es el resto del mensaje.
+     *
+     * @param message El mensaje a dividir.
+     * @return Una lista con el título y el cuerpo.
+     */
+    public static List<String> splitIntoTitleAndMessage(String message) {
+        if (message == null || message.isEmpty()) {
+            return List.of("", "");
+        }
+
+        int firstLineEnd = message.indexOf("\n");
+        if (firstLineEnd == -1) {
+            // Si no hay salto de línea todo el mensaje es el titulo
+            return List.of(message, "");
+        }
+
+        String title = message.substring(0, firstLineEnd).trim();
+        String body = message.substring(firstLineEnd + 1).trim();
+
+        return List.of(title, body);
+    }
 }
